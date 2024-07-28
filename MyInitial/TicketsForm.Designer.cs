02a54702891f23251466e19f586412dccce727cb
Roger <roger@cpinet.com.au>
2024-07-28 14:02:07 +0930

    Added the Ticketing.TicketsForm & TicketPrice.cs

---
+++ MyInitial/TicketsForm.Designer.cs
@@ -0,0 +1,212 @@
+﻿namespace Ticketing
+{
+    partial class TicketsForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblQuantity = new System.Windows.Forms.Label();
+            this.txtQuantity = new System.Windows.Forms.TextBox();
+            this.lblAmountDue = new System.Windows.Forms.Label();
+            this.lblAmount = new System.Windows.Forms.Label();
+            this.chkDiscount = new System.Windows.Forms.CheckBox();
+            this.radBalcony = new System.Windows.Forms.RadioButton();
+            this.radGeneral = new System.Windows.Forms.RadioButton();
+            this.radBox = new System.Windows.Forms.RadioButton();
+            this.grpSection = new System.Windows.Forms.GroupBox();
+            this.cmdCalculate = new System.Windows.Forms.Button();
+            this.txtDiscount = new System.Windows.Forms.TextBox();
+            this.txtDiscountLabel = new System.Windows.Forms.Label();
+            this.grpSection.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // lblQuantity
+            // 
+            this.lblQuantity.AutoSize = true;
+            this.lblQuantity.Location = new System.Drawing.Point(72, 74);
+            this.lblQuantity.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lblQuantity.Name = "lblQuantity";
+            this.lblQuantity.Size = new System.Drawing.Size(68, 20);
+            this.lblQuantity.TabIndex = 0;
+            this.lblQuantity.Text = "Quantity";
+            // 
+            // txtQuantity
+            // 
+            this.txtQuantity.Location = new System.Drawing.Point(166, 74);
+            this.txtQuantity.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.txtQuantity.Name = "txtQuantity";
+            this.txtQuantity.Size = new System.Drawing.Size(134, 26);
+            this.txtQuantity.TabIndex = 1;
+            // 
+            // lblAmountDue
+            // 
+            this.lblAmountDue.AutoSize = true;
+            this.lblAmountDue.Location = new System.Drawing.Point(366, 74);
+            this.lblAmountDue.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lblAmountDue.Name = "lblAmountDue";
+            this.lblAmountDue.Size = new System.Drawing.Size(99, 20);
+            this.lblAmountDue.TabIndex = 2;
+            this.lblAmountDue.Text = "Amount Due";
+            // 
+            // lblAmount
+            // 
+            this.lblAmount.AutoSize = true;
+            this.lblAmount.Location = new System.Drawing.Point(490, 82);
+            this.lblAmount.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lblAmount.Name = "lblAmount";
+            this.lblAmount.Size = new System.Drawing.Size(0, 20);
+            this.lblAmount.TabIndex = 3;
+            // 
+            // chkDiscount
+            // 
+            this.chkDiscount.AutoSize = true;
+            this.chkDiscount.Location = new System.Drawing.Point(166, 146);
+            this.chkDiscount.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.chkDiscount.Name = "chkDiscount";
+            this.chkDiscount.Size = new System.Drawing.Size(150, 24);
+            this.chkDiscount.TabIndex = 5;
+            this.chkDiscount.Text = "Senior / Student";
+            this.chkDiscount.UseVisualStyleBackColor = true;
+            this.chkDiscount.CheckStateChanged += new System.EventHandler(this.chkDiscount_CheckStateChanged);
+            // 
+            // radBalcony
+            // 
+            this.radBalcony.AutoSize = true;
+            this.radBalcony.Location = new System.Drawing.Point(39, 26);
+            this.radBalcony.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.radBalcony.Name = "radBalcony";
+            this.radBalcony.Size = new System.Drawing.Size(90, 24);
+            this.radBalcony.TabIndex = 6;
+            this.radBalcony.Text = "Balcony";
+            this.radBalcony.UseVisualStyleBackColor = true;
+            // 
+            // radGeneral
+            // 
+            this.radGeneral.AutoSize = true;
+            this.radGeneral.Checked = true;
+            this.radGeneral.Location = new System.Drawing.Point(39, 62);
+            this.radGeneral.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.radGeneral.Name = "radGeneral";
+            this.radGeneral.Size = new System.Drawing.Size(91, 24);
+            this.radGeneral.TabIndex = 7;
+            this.radGeneral.TabStop = true;
+            this.radGeneral.Text = "General";
+            this.radGeneral.UseVisualStyleBackColor = true;
+            // 
+            // radBox
+            // 
+            this.radBox.AutoSize = true;
+            this.radBox.Location = new System.Drawing.Point(39, 97);
+            this.radBox.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.radBox.Name = "radBox";
+            this.radBox.Size = new System.Drawing.Size(107, 24);
+            this.radBox.TabIndex = 8;
+            this.radBox.Text = "Box Seats";
+            this.radBox.UseVisualStyleBackColor = true;
+            // 
+            // grpSection
+            // 
+            this.grpSection.Controls.Add(this.radBox);
+            this.grpSection.Controls.Add(this.radGeneral);
+            this.grpSection.Controls.Add(this.radBalcony);
+            this.grpSection.Location = new System.Drawing.Point(78, 217);
+            this.grpSection.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.grpSection.Name = "grpSection";
+            this.grpSection.Padding = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.grpSection.Size = new System.Drawing.Size(208, 171);
+            this.grpSection.TabIndex = 9;
+            this.grpSection.TabStop = false;
+            this.grpSection.Text = "Section";
+            // 
+            // cmdCalculate
+            // 
+            this.cmdCalculate.Location = new System.Drawing.Point(441, 412);
+            this.cmdCalculate.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.cmdCalculate.Name = "cmdCalculate";
+            this.cmdCalculate.Size = new System.Drawing.Size(162, 54);
+            this.cmdCalculate.TabIndex = 10;
+            this.cmdCalculate.Text = "Calculate";
+            this.cmdCalculate.UseVisualStyleBackColor = true;
+            this.cmdCalculate.Click += new System.EventHandler(this.cmdCalculate_Click);
+            // 
+            // txtDiscount
+            // 
+            this.txtDiscount.Location = new System.Drawing.Point(514, 231);
+            this.txtDiscount.Name = "txtDiscount";
+            this.txtDiscount.Size = new System.Drawing.Size(100, 26);
+            this.txtDiscount.TabIndex = 11;
+            // 
+            // txtDiscountLabel
+            // 
+            this.txtDiscountLabel.AutoSize = true;
+            this.txtDiscountLabel.Location = new System.Drawing.Point(434, 234);
+            this.txtDiscountLabel.Name = "txtDiscountLabel";
+            this.txtDiscountLabel.Size = new System.Drawing.Size(72, 20);
+            this.txtDiscountLabel.TabIndex = 12;
+            this.txtDiscountLabel.Text = "Discount";
+            // 
+            // TicketsForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(912, 685);
+            this.Controls.Add(this.txtDiscountLabel);
+            this.Controls.Add(this.txtDiscount);
+            this.Controls.Add(this.cmdCalculate);
+            this.Controls.Add(this.grpSection);
+            this.Controls.Add(this.chkDiscount);
+            this.Controls.Add(this.lblAmount);
+            this.Controls.Add(this.lblAmountDue);
+            this.Controls.Add(this.txtQuantity);
+            this.Controls.Add(this.lblQuantity);
+            this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.Name = "TicketsForm";
+            this.Text = "Theater Ticket Purchase";
+            this.Load += new System.EventHandler(this.TicketsForm_Load);
+            this.grpSection.ResumeLayout(false);
+            this.grpSection.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblQuantity;
+        private System.Windows.Forms.TextBox txtQuantity;
+        private System.Windows.Forms.Label lblAmountDue;
+        private System.Windows.Forms.Label lblAmount;
+        private System.Windows.Forms.CheckBox chkDiscount;
+        private System.Windows.Forms.RadioButton radBalcony;
+        private System.Windows.Forms.RadioButton radGeneral;
+        private System.Windows.Forms.RadioButton radBox;
+        private System.Windows.Forms.GroupBox grpSection;
+        private System.Windows.Forms.Button cmdCalculate;
+        private System.Windows.Forms.TextBox txtDiscount;
+        private System.Windows.Forms.Label txtDiscountLabel;
+    }
+}
+
---
+++ MyInitial/TicketsForm.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace Ticketing
+{
+    public partial class TicketsForm : Form
+    {
+        TicketPrice mTicketPrice;
+        int mSection = 2;
+        int mQuantity = 0;
+        bool mDiscount = false;
+
+        public TicketsForm()
+        {
+            InitializeComponent();
+        }
+
+        private void TicketsForm_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void cmdCalculate_Click(object sender, EventArgs e)
+        {
+            mQuantity = int.Parse(txtQuantity.Text);
+
+            if (radBalcony.Checked)
+                { mSection = 1; }
+            if (radGeneral.Checked)
+                { mSection = 2; }
+            if (radBox.Checked)
+                { mSection = 3; }
+
+            mTicketPrice = new TicketPrice(mSection, mQuantity, mDiscount);
+
+            mTicketPrice.calculatePrice();
+            lblAmount.Text = System.Convert.ToString(mTicketPrice.AmountDue);
+        }
+
+        private void chkDiscount_CheckStateChanged(object sender, EventArgs e)
+        {
+            if (chkDiscount.Checked) { txtDiscount.Text = "5"; mDiscount = true; }
+            else { txtDiscount.Text = ""; mDiscount = false; }
+        }
+    }
+}
